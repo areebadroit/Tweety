4d9933713f5317322d6183fceebd0ffa69a6b250
Md Areeb Ansar <mdareebansar@gmail.com>
2021-09-08 03:09:12 +0530

    Added likes controller

--- src/models/comment.js
+++ src/models/comment.js
@@ -17,6 +17,10 @@ const commentSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Tweet',
         required: true
+    },
+    likes: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Like'
     }
 }, { timestamps: true });
 
---
+++ src/models/like.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    likeable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', , 'Comment']
+    }
+}, { timestamps: true });
+const Like = mongoose.model('like', likeSchema);
+module.exports = Like;
